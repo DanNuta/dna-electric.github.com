526c289f15fd7f18a1f2af66cecfdce1763cf650
Dan <dany.condurari@gmail.com>
2022-10-13 01:24:09 +0300

    ckuch

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig , splitVendorChunkPlugin} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [splitVendorChunkPlugin()],
   server: {
     host: true
-  }
+  },
+
+  build: {
+    
+  },
+
+  
+
+
 })
